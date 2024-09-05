83ef41104b2f699e312911a22ab998a972cf447d
roshan <roshan.kleinseetharaman@gmail.com>
2024-09-05 16:18:05 -0400

    backed up some files

---
+++ backup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git add ~/Documents/cs334/backup.sh
+git add ~/Documents/cs334/raspberrypi/ip.md
+git add ~/Documents/cs334/raspberrypi/.bashrc
+
+git commit -m "backed up some files"
+git push
