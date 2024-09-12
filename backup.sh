b5fdf947f6bca1b3c2e97f74f54af45f4095c9d8
roshan <roshan.kleinseetharaman@gmail.com>
2024-09-12 15:52:06 -0400

    [2024-09-12 15:52:06] backed up some files

--- backup.sh
+++ backup.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 cd ~/Documents/cs334
 cp /home/student334/.bashrc ~/Documents/cs334/raspberrypi
+cp /etc/systemd/system/thanks_koray_for_helping_us_with_this.service ~/Documents/cs334/raspberrypi
 hostname -I > ~/Documents/cs334/raspberrypi/ip.md
 pwd
 
-git add ~/Documents/cs334/backup.sh
-git add ~/Documents/cs334/raspberrypi/ip.md
-git add ~/Documents/cs334/raspberrypi/.bashrc
+git add -A
+# git add ~/Documents/cs334/backup.sh
+# git add ~/Documents/cs334/raspberrypi/ip.md
+# git add ~/Documents/cs334/raspberrypi/.bashrc
 
-git commit -m "backed up some files"
+git commit -m "[$(date '+%Y-%m-%d %H:%M:%S')] backed up some files"
 git push
---
+++ logs/lab2.md
@@ -0,0 +1,8 @@
+To ssh:
+ssh student334@10.67.67.136 -A
+Password: cpsc334!
+
+    https://docs.redhat.com/en/documentation/red_hat_enterprise_linux/9/html/using_systemd_unit_files_to_customize_and_optimize_your_system/assembly_working-with-systemd-unit-files_working-with-systemd#proc_creating-custom-unit-files_assembly_working-with-systemd-unit-files
+    
+We created a systemd unit file called:
+    thanks_koray_for_helping_us_with_this.service
