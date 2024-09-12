113542c9a3519efe33e2c2707b1bf9deb309a14e
roshan <roshan.kleinseetharaman@gmail.com>
2024-09-12 15:49:21 -0400

    backed up some files

--- backup.sh
+++ backup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+cd ~/Documents/cs334
+cp /home/student334/.bashrc ~/Documents/cs334/raspberrypi
+hostname -I > ~/Documents/cs334/raspberrypi/ip.md
+pwd
 
 git add ~/Documents/cs334/backup.sh
 git add ~/Documents/cs334/raspberrypi/ip.md
