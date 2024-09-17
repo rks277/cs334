d5645666741d1b9e4a1be08af05d8b5eb7ae9f93
Roshan K-S <97183200+rks277@users.noreply.github.com>
2024-09-16 22:06:49 -0400

    make properties dynamic

--- generative_art/space_battle.js
+++ generative_art/space_battle.js
@@ -1,100 +1,90 @@
 let spaceshipLayer;
 let spaceshipHeight, spaceshipWidth, spaceshipLayerDepth;
 let tapered;
-let color1, color2, color3;
-let lerpedColor, lerpedColor2;
-let angle = 0;
+let c1, c2, c3;
+let white, color_;
 let posX2, posY2;
 let speedX2, speedY2;
 let scaleFactor;
 let points = [];
 let accel = 0.9;
 let bounce = -1;
+let numAliens = 5;
+let alienPositions = [];
 
 function setup() {
-  createCanvas(900, 900);
+  createCanvas(windowWidth, windowHeight);
   
-  // Canvas for each spaceship layer
-  spaceshipLayer = createGraphics(400, 400);
+  if (width * height < 1000) {numAliens = 0}
+  else {numAliens = int(width / 150)}
+  
+  alien(width * 0.2, height * 0.2);
+  spaceshipLayer = createGraphics(width * 0.4, height * 0.4);
   resetSpaceshipLayer();
-  spaceshipLayerDepth = int(random(40, 70)); 
-
-  color1 = color(250, 120, 50, 50);  // Orange for explosions
-  color2 = color(255);  // White
-  color3 = color(0);    // Black
-  
-  lerpedColor = lerpColor(color1, color2, 0.8);
-  lerpedColor2 = lerpColor(color1, color3, 0.8);
-  
+  spaceshipLayerDepth = int(random(width * 0.04, height * 0.07)); 
+  c1 = color(250, 120, 50, 50);
+  c2 = color(255);
+  c3 = color(0);
+  white = lerpColor(c1, c2, 0.8);
+  color_ = lerpColor(c1, c3, 0.8);
   imageMode(CENTER);
-  
   background(0);
-  
   posX2 = 0;
-  posY2 = 900;
-
+  posY2 = height;
   speedX2 = random(-1, 1); 
   speedY2 = random(-3, -1);
-
-  // Drawing the stars in the background
   for (let i = 0; i < width; i += 40) {
     for (let j = 0; j < height; j += 40) {
       strokeWeight(random(0.5, 2));
-      stroke(lerpedColor);
+      stroke(white);
       point(i + random(-30, 30), j + random(-30, 30));
     }
+  }
+  for (let i = 0; i < numAliens; i++) {
+    alienPositions.push(createVector(random(width), random(height)));
   }
 }
 
 function draw() {
-  
-  // Draw the green flow field
-  drawFlowField();
-  
-  
   if (frameCount % spaceshipLayerDepth == 0) resetSpaceshipLayer();
-  
   if (tapered > 50) scaleFactor *= 0.995;
   
   posX2 += speedX2 + 4;
   posY2 += speedY2;
-
-  let displacement2 = map(cos(angle), -1, 1, 0, PI);
-  tower(displacement2);
-
-  lerpedColor2 = lerpColor(color1, color3, 0.8);
-
-  angle += 0.1;
+  spaceship();
+  color_ = lerpColor(c1, c3, 0.8);
 
   // Drawing the spaceship on the main canvas
   blendMode(DIFFERENCE);
   image(spaceshipLayer, posX2, posY2, width * scaleFactor, height * scaleFactor);
 
-  // Update and draw explosions
+  // Update and draw alien spaceships
   for (let p of points) {
-    if (p.y > height || p.y < 0) p.ydrift *= bounce;
-    if (p.x > width || p.x < 0) p.xdrift *= bounce;
-
     let diam = max(0, 9 + 40 / p.age);
-    fill(255);
-    stroke(color(0, 128, 100));
+    fill(color(0, 255, 0));
+    stroke(color(106, 22, 184));
     ellipse(p.x, p.y, diam, diam);
-
     p.x += p.xdrift;
     p.y += p.ydrift;
     p.xdrift *= accel;
     p.ydrift = p.ydrift * accel;
     p.age++;
   }
+  if (frameCount == 300) {
+    for (let pt of alienPositions) {
+      alien(pt.x, pt.y);
+    }
+  }
+  if (frameCount == 350) noLoop();
 }
 
-function tower(move) {
+function spaceship() {
   spaceshipLayer.stroke(255);
   spaceshipLayer.strokeWeight(1);
   
-  for (let i = 0; i < spaceshipLayer.width; i += 100) {
-    for (let j = spaceshipLayer.height; j > 0; j -= 50) {
-      spaceshipLayer.fill(lerpedColor2);
+  for (let i = 0; i < spaceshipLayer.width; i += spaceshipLayer.width * 0.25) {
+    for (let j = spaceshipLayer.height; j > 0; j -= spaceshipLayer.height * 0.125) {
+      spaceshipLayer.fill(color_);
       spaceshipLayer.rect(i, j, spaceshipWidth, spaceshipWidth);
     }
   }
@@ -104,16 +94,12 @@ function resetSpaceshipLayer() {
   tapered = random(0,100);
   scaleFactor = random(0.2, 0.5);
   spaceshipLayer.clear();
-  spaceshipHeight = random(20, 100);
-  spaceshipWidth = random(20, 100);
+  spaceshipHeight = random(0.1, 0.2) * spaceshipLayer.height;
+  spaceshipWidth = random(0.1, 0.2) * spaceshipLayer.width;
 }
 
-function mousePressed() {
-  goBoom(mouseX, mouseY);
-}
-
-function goBoom(xpos, ypos) {
-  for (let i = 0; i < 20; i++) {
+function alien(xpos, ypos) {
+  for (let i = 0; i < random(10, 20); i++) {
     points.push({ 
       x: xpos + random(-2, 2), 
       y: ypos + random(-2, 2), 
@@ -121,20 +107,5 @@ function goBoom(xpos, ypos) {
       ydrift: random(-10, 10),
       age: 1
     });
-  }
-}
-
-// Function to draw the flow field starting from the origin
-function drawFlowField() {
-  stroke(0, 255, 0);  // Set the flow field to green
-  strokeWeight(1);
-  
-  for (let x = 0; x < width; x += 20) {
-    for (let y = 0; y < height; y += 20) {
-      let angle = atan2(y - height / 2, x - width / 2); // Calculate angle based on the origin
-      let vec = p5.Vector.fromAngle(angle); // Create a vector from the angle
-      vec.setMag(15); // Set the length of the vector
-      line(x, y, x + vec.x, y + vec.y); // Draw the flow line
-    }
   }
 }
