709bd4cb493afd7b697142d45e054c3a5388ee0e
Roshan K-S <97183200+rks277@users.noreply.github.com>
2024-11-13 21:40:55 -0500

    add animations

--- module4/task2/octopus.ino
+++ module4/task2/octopus.ino
@@ -3,6 +3,7 @@
 
 // Setup
 #define NUM_SERVOS 8
+#define NUM_ANIMATIONS 5
 
 // Create servo arrays
 Servo servos[NUM_SERVOS];
@@ -75,9 +76,102 @@ int moveAllArmsDown() {
 }
 
 // Animations
+void waveMotion(int amplitude, int speed) {
+    for (int angle = 0; angle <= amplitude; angle += speed) {
+        for (int i = 0; i < NUM_SERVOS; i++) {
+            int waveAngle = angle + (i * speed);
+            waveAngle = waveAngle % 180;
+            servos[i].write(waveAngle);
+            servoPositions[i] = waveAngle;
+        }
+        delay(100);
+    }
+}
+
+void sequential(int stepAngle, int liftAngle) {
+    for (int i = 0; i < NUM_SERVOS; i++) {
+        rotate(stepAngle);
+        if (moveArm(i, liftAngle) < 0) {
+            continue;
+        }
+        delay(200);
+        moveArm(i, -liftAngle);
+    }
+}
+
+void bounce(int bounceAngle, int stepAngle, int bounces) {
+    for (int b = 0; b < bounces; b++) {
+        for (int i = 0; i < NUM_SERVOS; i++) {
+            if (moveArm(i, bounceAngle) < 0) {
+                continue;
+            }
+        }
+        delay(200);
+
+        for (int i = 0; i < NUM_SERVOS; i++) {
+            moveArm(i, -bounceAngle);
+        }
+        delay(200);
+    }
+}
+
+void spiral(int maxStepAngle, int sweepAngle) {
+    for (int angle = 0; angle < maxStepAngle; angle += 20) {
+        rotate(angle);
+
+        for (int i = 0; i < NUM_SERVOS; i++) {
+            if (moveArm(i, sweepAngle - servoPositions[i]) < 0) {
+                continue;
+            }
+        }
+        delay(200);
+
+        for (int i = 0; i < NUM_SERVOS; i++) {
+            moveArm(i, -sweepAngle);
+        }
+        delay(200);
+    }
+}
+
+void cascade(int cascadeAngle, int stepAngle) {
+    for (int i = 0; i < NUM_SERVOS; i++) {
+        if (moveArm(i, cascadeAngle - servoPositions[i]) < 0) {
+            continue;
+        }
+        delay(100);
+        rotate(stepAngle);
+    }
+
+    for (int i = 0; i < NUM_SERVOS; i++) {
+        moveArm(i, -cascadeAngle);
+    }
+    delay(500);
+}
 
 void loop() {
-  
-
-  delay(1000);
+    int animationIndex = random(0, NUM_ANIMATIONS);
+    switch (animationIndex) {
+        case 0:
+            Serial.println("Playing wave motion");
+            waveMotion(180, 10);
+            break;
+        case 1:
+            Serial.println("Playing sequential motion");
+            sequentialLift(10, 30);
+            break;
+        case 2:
+            Serial.println("Playing bounce animation");
+            bounceAnimation(30, 5);
+            break;
+        case 3:
+            Serial.println("Playing spiral sweep");
+            spiralSweep(60, 45);
+            break;
+        case 4:
+            Serial.println("Playing cascade effect");
+            cascadeEffect(90, 15);
+            break;
+    }
+    
+    delay(1000);
 }
